bffd093056b4d010118947d2aae08fcf6ad2eaa8
Marat Fomin <skinner.fomin@gmail.com>
2024-07-08 17:10:08 +0400

    Fixed echo handler

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -9,7 +9,3 @@ rt = Router()
 @rt.message()
 async def echo_handler(message: Message, state: FSMContext) -> None:
     await message.answer('Не понял, нажмите на кнопку ниже, либо выполните команду /help')
-    await state.update_data({str(message.message_id): message.text})
-    data = await state.get_data()
-    state = await state.get_state()
-    print(data, state, sep='\n')
